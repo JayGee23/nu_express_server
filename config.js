7a5d5e46a600bb90fa16a420cd7e35ea0e70a7a2
JayGee <jayguarino@gmail.com>
2023-04-11 16:33:06 -0700

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const facebookKey = require('./facebookKey');
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3451708501825823',
+        clientSecret: facebookKey
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -71,4 +71,13 @@ router.get('/logout', cors.corsWithOptions, (req, res, next) => {
 });
 
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'apllication/json');
+    res.json({ success: true, token: token, status: 'You are succesfully logged in!' });
+  }
+});
+
 module.exports = router;
